0e92e55098a7f7f83cdd417b52f18c172bd77937
David J Kelley <4581942+DavidJKelley@users.noreply.github.com>
2025-08-21 12:03:29 -0700

    Update D597 Task 2 - Part 2 D2plus.bat

--- D597-DataManagement/Task2/D597 Task 2 - Part 2 D2plus.bat
+++ D597-DataManagement/Task2/D597 Task 2 - Part 2 D2plus.bat
@@ -1,1 +1,11 @@
+@echo off
 
+REM "C:\Program Files\MongoDB\Server\7.0\bin\mongoimport.exe" 
+ 
+set DB_NAME=D597Task2
+set COLLECTION_NAME=groceries
+set FILE_PATH=C:\Scenario2\Task2Scenario2Dataset2_Groceries_dataset.json
+
+"C:\Program Files\MongoDB\Server\7.0\bin\mongoimport.exe" --db %DB_NAME% --collection %COLLECTION_NAME% --file %FILE_PATH% --jsonArray
+
+pause

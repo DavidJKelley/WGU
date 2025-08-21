6d1b4c7993bb9248f028de19fe0818136ffb73ab
David J Kelley <4581942+DavidJKelley@users.noreply.github.com>
2025-08-21 12:02:15 -0700

    Update D597 Task 2 - Part 2 D1n2.bat

--- D597-DataManagement/Task2/D597 Task 2 - Part 2 D1n2.bat
+++ D597-DataManagement/Task2/D597 Task 2 - Part 2 D1n2.bat
@@ -1,1 +1,12 @@
 
+@echo off
+
+REM "C:\Program Files\MongoDB\Server\7.0\bin\mongoimport.exe" 
+ 
+set DB_NAME=D597Task2
+set COLLECTION_NAME=cosmetics
+set FILE_PATH=C:\Scenario2\Task2Scenario2Dataset1_cosmetics.json
+
+"C:\Program Files\MongoDB\Server\7.0\bin\mongoimport.exe" --db %DB_NAME% --collection %COLLECTION_NAME% --file %FILE_PATH% --jsonArray
+
+pause
